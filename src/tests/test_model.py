ef63e31acb1125a15853f3b8529cea20d4ecf87c
plbalmeida <plbalmeida@usp.br>
2024-04-30 20:10:26 -0300

    fix(*): correção da para o bandit e ajuste no ci.yml

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -6,6 +6,6 @@ def test_predict():
     test_input = [5.9, 3.0, 5.1, 1.8]
     prediction = model.predict(test_input)
     expected_output = 2
-    assert prediction == expected_output, f"""
+    assert prediction == expected_output, f"""  # nosec
         Expected prediction {expected_output}, but got {prediction}
     """
