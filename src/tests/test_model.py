947cd22562c8c23fc462a36150b38c9a6644fd84
plbalmeida <plbalmeida@usp.br>
2024-04-30 20:34:37 -0300

    fix(src/tests/test_model.py): import do .pkl

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -1,8 +1,9 @@
+import joblib
 import pytest  # noqa: F401
-import model
 
 
 def test_predict():
+    model = joblib.load('iris_model.pkl')
     test_input = [5.9, 3.0, 5.1, 1.8]
     prediction = model.predict(test_input)
     expected_output = 2
