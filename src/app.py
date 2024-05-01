8e88c5e52537cb025dfc3c2c1cc94d1679d44f6c
plbalmeida <plbalmeida@usp.br>
2024-04-30 22:18:12 -0300

    test(src/app.py): garantindo o código de status 400 para solicitações inválidas para /predict

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import joblib
-import os
 from flask import Flask, request, jsonify
 
 
@@ -12,12 +11,9 @@ model = joblib.load('iris_model.pkl')
 def predict():
     try:
         data = request.get_json()
+        if 'features' not in data:
+            return jsonify({'error': 'Missing features'}), 400
         predictions = model.predict([data['features']])
         return jsonify({'prediction': int(predictions[0])})
     except Exception as e:
-        return jsonify({'error': str(e)})
-
-
-if __name__ == '__main__':
-    debug_mode = os.getenv('FLASK_DEBUG', 'False') == 'True'
-    app.run(debug=debug_mode, host='127.0.0.1', port=5000)
+        return jsonify({'error': str(e)}), 400
